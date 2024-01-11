ae30c626e5261a16ee75d3f35008cbd664d08184
Haydn Comley <haydncomley@gmail.com>
2024-01-11 00:50:43 +0000

    PWA Support

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
-
+import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
 	build: {
@@ -13,11 +13,9 @@ export default defineConfig({
 	plugins: [
 		react(),
 		viteTsconfigPaths(),
-		svgrPlugin()
+		svgrPlugin(),
+		VitePWA({ registerType: 'autoUpdate' })
 	],
-	server: {
-		open: true,
-	},
 	test: {
 		coverage: {
 			all: true,
